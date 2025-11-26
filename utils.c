5478e39d8172ef9478cde96542d07c27d0c98459
ismael <ismaelbsd.pro@gmail>
2025-11-26 18:05:29 +0100

    task0

---
+++ utils.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - write a character to stdout
+ * @c: character to print
+ *
+ * Return: number of characters written (1)
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
